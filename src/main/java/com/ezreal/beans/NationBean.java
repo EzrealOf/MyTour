42ddd01b0a854d4ba51228f5b56335a1d57d0b28
EzrealOf <564699585@qq.com>
2017-04-28 13:54:33 +0800

    third

---
+++ src/main/java/com/ezreal/beans/NationBean.java
@@ -0,0 +1,53 @@
+package com.ezreal.beans;
+
+/**
+ * Created by Ezreal# on 2017/4/27.
+ */
+public class NationBean {
+    private int nationid;
+    private  String nationname;
+    private  String nationurl;
+    private  String nationdes;
+
+    public int getNationid() {
+        return nationid;
+    }
+
+    public void setNationid(int nationid) {
+        this.nationid = nationid;
+    }
+
+    public String getNationname() {
+        return nationname;
+    }
+
+    public void setNationname(String nationname) {
+        this.nationname = nationname;
+    }
+
+    public String getNationurl() {
+        return nationurl;
+    }
+
+    public void setNationurl(String nationurl) {
+        this.nationurl = nationurl;
+    }
+
+    public String getNationdes() {
+        return nationdes;
+    }
+
+    public void setNationdes(String nationdes) {
+        this.nationdes = nationdes;
+    }
+
+    @Override
+    public String toString() {
+        return "NationBean{" +
+                "nationid=" + nationid +
+                ", nationname='" + nationname + '\'' +
+                ", nationurl='" + nationurl + '\'' +
+                ", nationdes='" + nationdes + '\'' +
+                '}';
+    }
+}
